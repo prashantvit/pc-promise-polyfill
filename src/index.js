9ac6a85242bdb9f0a2ceb02a7df28d13dd9b8d16
Prashant Choudhary <prashant.srs@gmail.com>
2018-12-12 15:29:17 +0530

    Chaining Incomplete

--- src/index.js
+++ src/index.js
@@ -1,26 +1,53 @@
 function pcPromise(promiseFn) {
   var that = this;
+  let nextData;
   this.resolve = function(data) {
-    console.log("resolved", data);
-    that.thenFn(data);
+    console.log("resolved", data, that.thenFn);
+    if (that.thenFn) {
+      nextData = that.thenFn(data);
+      // console.log("H1");
+      that.thenFn = null;
+      // console.log("H2");
+
+      // console.log(nextData, "next");
+      if (nextData) {
+        setTimeout(() => that.resolve(nextData));
+      }
+    }
   };
-  this.reject = function(data) {
-    console.log("rejected", data);
+  this.reject = function(error) {
+    // console.log("rejected", error);
+    that.catchFn(error);
   };
   this.promiseFn = promiseFn;
   this.then = function(thenFn) {
+    console.log("then called");
     that.thenFn = thenFn;
+    return that;
+  };
+  this.catch = function(catchFn) {
+    that.catchFn = catchFn;
+    return that;
   };
   setTimeout(() => this.promiseFn(this.resolve, this.reject));
 }
 
 var test = new pcPromise(function(resolve, reject) {
-  console.log("Proimise Here");
+  // console.log("Proimise Here");
   resolve(1);
-  reject(2);
-}).then(data => {
-  console.log(data, "then data");
-});
+  // reject(2);
+})
+  .then(data => {
+    console.log(data, "then data");
+    return 2 * data;
+  })
+  .then(data => {
+    console.log(data, "then data");
+    return 3 * data;
+  })
+  .catch(error => {
+    console.log(error, "catched error");
+  });
 
 console.log(test);
 document.getElementById("app").innerHTML = JSON.stringify(test);
